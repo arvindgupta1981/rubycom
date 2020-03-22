71eca27e95ffca96526b02494fb0ca9055b71d16
arvindgupta1981 <arvind.mannindia@gmail.com>
2020-03-22 16:22:26 +0530

    commit

--- rubicon/src/main/java/com/rubicon/water/order/dao/WaterOrderState.java
+++ rubicon/src/main/java/com/rubicon/water/order/dao/WaterOrderState.java
@@ -2,106 +2,123 @@ package com.rubicon.water.order.dao;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.rubicon.water.order.dto.WaterOrder;
 import com.rubicon.water.order.dto.WaterStatus;
 
+/* @
+ * This class is maintaining the state of orders in Map. When an order arrives, it is saved in map.
+ * When service needs to fetch an order, it is fetched from map.
+ * When services wants to cancel an order, it fetches that order from map and changes the status of the order to cancel. 
+ */
 public class WaterOrderState {
 	
-	public static Map<String,List<WaterOrder>> waterOrderMap;
-	
+	public static final Logger logger = LoggerFactory.getLogger(WaterOrderState.class.getName());
+
+	public static Map<String, List<WaterOrder>> waterOrderMap;
+
 	static {
-		waterOrderMap = new HashMap<>();
+		waterOrderMap = new ConcurrentHashMap<>();
 	}
 
 	public static Map<String, List<WaterOrder>> getWaterOrderMap() {
 		return waterOrderMap;
 	}
 
-	public static void createWaterOrder(WaterOrder waterOrder) {
-		
-		if (waterOrderMap.containsKey(String.valueOf(waterOrder.getFarmId()))){
+	public static boolean createWaterOrder(final WaterOrder waterOrder) {
+
+		boolean orderCreated = false;
+		if (waterOrderMap.containsKey(String.valueOf(waterOrder.getFarmId()))) {
 			List<WaterOrder> waterOrderList = waterOrderMap.get(String.valueOf(waterOrder.getFarmId()));
 			boolean orderOverlap = checkIfOrdersOverlap(waterOrderList, waterOrder);
-			if(!orderOverlap) {
+			if (!orderOverlap) {
+				waterOrder.setStaus(WaterStatus.REQUESTED.toString());
 				waterOrderList.add(waterOrder);
+				orderCreated = true;
+				logger.info("Order created and saved in map.");
 			}
 		} else {
+			waterOrder.setStaus(WaterStatus.REQUESTED.toString());
 			List<WaterOrder> waterOrderList = new ArrayList<>();
 			waterOrderList.add(waterOrder);
 			waterOrderMap.put(String.valueOf(waterOrder.getFarmId()), waterOrderList);
+			orderCreated = true;
+			logger.info("Order created and saved in map.");
 		}
-	}
 		
-	private static boolean checkIfOrdersOverlap(List<WaterOrder> waterOrderList, WaterOrder newWaterOrder) {
+		return orderCreated;
+	}
+
+	/* Checks if the received order does not overlap with existing order 
+	i.e. order does not start when another order is in progress at the same time.
+	It only checks for those orders which are not cancelled.*/
+	private static boolean checkIfOrdersOverlap(final List<WaterOrder> waterOrderList, final WaterOrder newWaterOrder) {
 		
 		LocalDateTime orderDateTime = newWaterOrder.getDateTime();
 		boolean orderOverlap = false;
-		for(WaterOrder wo : waterOrderList) {
-			LocalDateTime finishTime = wo.getOrderFinishTime(wo.getDateTime(), wo.getDuration());
-			if ((orderDateTime.isAfter(wo.getDateTime()) && orderDateTime.isBefore(finishTime)) || orderDateTime.equals(wo.getDateTime())) {
-				System.out.println("Orders overlap for the same farm and will not be created.");
-				return true;
-			} else {
-				 System.out.println("Order do not overlap for the same farm."); 
-				 orderOverlap = false;
+		for (WaterOrder wo : waterOrderList) {
+			logger.info("datetime======."+ wo.getDateTime());
+			if(!wo.getStaus().equals(WaterStatus.CANCELLED.toString())) {
+				LocalDateTime finishTime = wo.getOrderFinishTime(wo.getDateTime(), wo.getDuration());
+				if ((orderDateTime.isAfter(wo.getDateTime()) && orderDateTime.isBefore(finishTime))
+						|| orderDateTime.equals(wo.getDateTime())) {
+					logger.info("Orders overlap for the same farm and will not be created.");
+					return true;
+				} 
 			}
 		}
+		logger.info("Order do not overlap and will be created.");
 		return orderOverlap;
 	}
 
-	public static List<WaterOrder> fetchWaterOrder(WaterOrder waterOrder) {
-		
-		if (waterOrderMap.containsKey(String.valueOf(waterOrder.getFarmId()))){
-			List<WaterOrder> waterOrderList = waterOrderMap.get(String.valueOf(waterOrder.getFarmId()));
+	public static List<WaterOrder> fetchWaterOrder(final WaterOrder waterOrder) {
+		List<WaterOrder> waterOrderList = new ArrayList<>();
+		if (waterOrderMap.containsKey(String.valueOf(waterOrder.getFarmId()))) {
+			waterOrderList = waterOrderMap.get(String.valueOf(waterOrder.getFarmId()));
+			logger.info("Orders successfully fetched from map.");
 			return waterOrderList;
 		} else {
-			System.out.println("Order with the farm id does not exist");
-			 return null;
+			logger.info("Order with the farm id "+  waterOrder.getFarmId() + "does not exist. ");
+			return waterOrderList;
 		}
-		
+
 	}
-	
-	public static List<WaterOrder> cancelWaterOrder(WaterOrder waterOrder) {
-		
-		int cancelledOrdersCount = 0;
+
+	/*Cancel those orders which are not in delievered status. 
+	 */
+	public static List<WaterOrder> cancelWaterOrder(final WaterOrder waterOrder) {
+
 		List<WaterOrder> cancelledWaterOrderList = new ArrayList<>();
-		
-		if (waterOrderMap.containsKey(String.valueOf(waterOrder.getFarmId()))){
+		if (waterOrderMap.containsKey(String.valueOf(waterOrder.getFarmId()))) {
+
 			List<WaterOrder> waterOrderList = waterOrderMap.get(String.valueOf(waterOrder.getFarmId()));
-			for (WaterOrder wo: waterOrderList) {
-				 if(!wo.getStaus().equals(WaterStatus.DELIEVERED)) {
-					 wo.setStaus(WaterStatus.CANCELLED.toString());
-					 cancelledWaterOrderList.add(wo);
-					 waterOrderList.remove(wo);
-					 cancelledOrdersCount++;
-					 if(waterOrderList.isEmpty()) {
-						 waterOrderMap.remove(String.valueOf(waterOrder.getFarmId()));
-					 }
-				 } else {
-					 System.out.println("Order with the farm id can not be cancelled as it is delieverd");
-				 }
-			}
-			
+
+			cancelledWaterOrderList = waterOrderList.stream()
+					.filter(wo -> !(wo.getStaus().equals(WaterStatus.DELIEVERED.toString())))
+					.collect(Collectors.toList());
+
+			cancelledWaterOrderList.forEach(wo -> wo.setStaus(WaterStatus.CANCELLED.toString()));
+
+			logger.info("Number of cancelled orders" + cancelledWaterOrderList.size());
 		} else {
-			System.out.println("No order found with the farmId");
+			logger.info("Order with the farm id "+  waterOrder.getFarmId() + "does not exist. ");
 		}
-		
-		System.out.println("Number of cancelled orders" + cancelledOrdersCount);
-		
+
 		return cancelledWaterOrderList;
 	}
-	
+
 	public static void getOrdersToStartProcessing() {
-		
+
 		System.out.println("Started Processing");
-		
+
 		/*
 		 * List<List<WaterOrder>> values =
 		 * getWaterOrderMap().values().stream().collect(Collectors.toList());
@@ -119,10 +136,9 @@ public class WaterOrderState {
 		 * waterOrderList.stream() .forEach(wo ->
 		 * wo.setStaus(WaterStatus.IN_PROGRESS.toString()));
 		 */
-		
-		
-	//	System.out.println("Changed status of task to progress");
-		
+
+		// System.out.println("Changed status of task to progress");
+
 		for (Entry<String, List<WaterOrder>> entry : getWaterOrderMap().entrySet()) {
 
 			List<WaterOrder> waterOrderLs = entry.getValue();
@@ -138,8 +154,6 @@ public class WaterOrderState {
 
 			}
 		}
-		 
-		
-		
+
 	}
 }
--- rubicon/src/main/java/com/rubicon/water/order/endpoint/WaterService.java
+++ rubicon/src/main/java/com/rubicon/water/order/endpoint/WaterService.java
@@ -7,9 +7,12 @@ import java.util.List;
 import org.quartz.JobBuilder;
 import org.quartz.JobDataMap;
 import org.quartz.JobDetail;
+import org.quartz.JobKey;
 import org.quartz.SchedulerException;
 import org.quartz.Trigger;
 import org.quartz.TriggerBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,7 +29,8 @@ import com.rubicon.water.order.jobs.WaterOrderJob;
 @RestController
 @RequestMapping(path = "/waterservice")
 public class WaterService {
-	
+	public static final Logger logger = LoggerFactory.getLogger(WaterService.class.getName());
+
 	//public static final Logger logger = Logger.getLogger(WaterService.class);
 	@Autowired
     private SchedulerFactoryBean schedulerFactoryBean;
@@ -35,58 +39,68 @@ public class WaterService {
     public String hello() {
 		schedulerFactoryBean.getScheduler();
         return "Water service is up.";
-    }
-	
+    }	
 	
 	@PostMapping(path= "/create", produces = "application/xml")
     public WaterOrder createOrder(@RequestBody WaterOrder waterOrder) throws SchedulerException {
 		
-		//logger.info("Creating an Order");
-		System.out.println ("Entered create service " + waterOrder.getDateTime());
-		
-		String jobKey = waterOrder.getFarmId() + waterOrder.getDateTime().toString();
-		JobDataMap map = new JobDataMap();
-		map.put("job", waterOrder);
-		
-		JobDetail detail = JobBuilder.newJob(WaterOrderJob.class).withIdentity(jobKey).usingJobData(map) .build();
-
-		
+		logger.info("Creating an Order");		
 		waterOrder.setStaus(WaterStatus.REQUESTED.toString());
-		WaterOrderState.createWaterOrder(waterOrder);
-		Trigger trigger = trigger = TriggerBuilder
-                .newTrigger()
-                .startAt(Date.from(waterOrder.getDateTime().atZone(ZoneId.systemDefault()).toInstant()))
-                .endAt(waterOrder.getOrderFinishTime())
-                .build();
+		Boolean orderCreated = WaterOrderState.createWaterOrder(waterOrder);
+		if (orderCreated) {
+			String jobKeyName = getJobKeyName(waterOrder);
+			JobDataMap map = new JobDataMap();
+			map.put("job", waterOrder);			
+			JobDetail detail = JobBuilder.newJob(WaterOrderJob.class).withIdentity(jobKeyName, "rubicon").usingJobData(map) .build();
+			logger.info("group ::" + detail.getKey().getGroup());
+			logger.info("name: " + detail.getKey().getName());
+			Trigger trigger = TriggerBuilder
+	                .newTrigger()
+	                .startAt(Date.from(waterOrder.getDateTime().atZone(ZoneId.systemDefault()).toInstant()))
+	                .endAt(waterOrder.getOrderFinishTime())
+	                .build();
+			schedulerFactoryBean.getScheduler().scheduleJob(detail, trigger);
+			logger.debug("order successfully Created with staus : " + waterOrder.getStaus().toString());
+
+		} else {
+			throw new UnsupportedOperationException("Order not created as the orders overlap");
+		}
 		
-		schedulerFactoryBean.getScheduler().scheduleJob(detail, trigger);
-		//logger.debug("order successfully Created");
-		//logger.debug("order status is: " +  waterOrder.getStaus().toString());
+		
+		logger.debug("order successfully Created");
+		logger.debug("order status is: " +  waterOrder.getStaus().toString());
 		
     	return waterOrder;
     }
-    
-	 
 
-	@PostMapping(path= "/cancel", produces = "application/xml")
-    public List<WaterOrder> cancelOrder(@RequestBody WaterOrder waterOrder) {
-		
-		 //logger.info("request to cancel an order recieved");
+	@PostMapping(path= "/cancel")
+    public String cancelOrder(@RequestBody WaterOrder waterOrder) throws SchedulerException {		
+		logger.info("request to cancel an order recieved");
 		List<WaterOrder> waterOrderList = WaterOrderState.cancelWaterOrder(waterOrder);
-		
-		return waterOrderList;
-		 
+		String jobKeyName = getJobKeyName(waterOrder);
+		JobKey jobKey = new JobKey(jobKeyName, "rubicon");
+		if(waterOrderList.isEmpty() || !schedulerFactoryBean.getScheduler().checkExists(jobKey)) {
+			logger.debug("order can not be cancelled");
+			throw new UnsupportedOperationException("Order can not be cancelled either it does not exist or in delievered status");
+		} else {			
+			schedulerFactoryBean.getScheduler().deleteJob(jobKey);
+			logger.debug("order successfully cancelled and staus changed to cancelled");
+		}
+		return "Order Cancelled";		 
     }
 
 
     @PostMapping(path = "/find", produces = "application/xml")
-    public List<WaterOrder> getExistingOrders(WaterOrder waterOrder) {
-    	
-    	//logger.info("request to fetch an order recieved");
-    	
-    	List<WaterOrder> waterOrderList = WaterOrderState.fetchWaterOrder(waterOrder);
-    	
-    	//logger.info("order successfully fetched");
+    public List<WaterOrder> getExistingOrders(WaterOrder waterOrder) {    	
+    	logger.info("request to fetch an order recieved");    	
+    	List<WaterOrder> waterOrderList = WaterOrderState.fetchWaterOrder(waterOrder);    	
+    	logger.info("order successfully fetched");
     	return waterOrderList;
     }
+    
+
+	private String getJobKeyName(WaterOrder waterOrder) {
+		String jobKeyName = waterOrder.getFarmId() + waterOrder.getDateTime().toString();
+		return jobKeyName;
+	}
 }
